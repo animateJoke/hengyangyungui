462a7f49dfcb3d8c60049e1617ca75f0e0523487
ssty <496927765@qq.com>
2018-03-22 12:27:44 +0800

    gai

--- src/management/js/table.js
+++ src/management/js/table.js
@@ -11,12 +11,12 @@ require(["config"], function () {
                 for (var i = 0; i < res.length; i++) {
                     console.log(res[i]);
                     var str = "";
-                    str += '<tr data-info="' + res[i].id + '" class="list">';
+                    str += '<tr data-info="' + res[i].u_id + '" class="list">';
                     str += '<td><input type="checkbox"></td>';
                     str += ' <td>' + (i+1) + '</td>';
                     str += ' <td><a href="#">' + res[i].u_name1 + '</a></td>';
-                    str += ' <td>' + res[i].email + '</td>';
-                    str += '<td>' + res[i].tel + '</td>';
+                    str += ' <td>' + res[i].u_email + '</td>';
+                    str += '<td>' + res[i].u_tel + '</td>';
                     str += '<td>' + res[i].u_post + '</td>';
                     str += '<td><a href="#" class="delete">删除</a></td>';
                     str += '</tr>';
@@ -60,7 +60,7 @@ require(["config"], function () {
                 $("#tab").html("");
                 for (var i = 0; i < res.length; i++) {
                     var str = '';
-                    str += '<tr data-info="' + res[i].id + '" class="list">';
+                    str += '<tr data-info="' + res[i].u_id + '" class="list">';
                     str += '<td><input type="checkbox"></td>';
                     str += ' <td>' + (i+1 )+ '</td>';
                     str += ' <td><a href="#">' + res[i].u_name1 + '</a></td>';
